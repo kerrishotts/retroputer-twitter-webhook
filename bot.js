16db6faf7c7de2100e7cb5b000a8731c65ce1899
Glitch (account-activity-dashboard-beta) <none>
2018-05-18 22:09:44 +0000

    🗻🍌 Checkpoint ./package.json:178972/2054 ./bot.js:178972/3759 ./twitter.js:178972/5527 ./helpers.js:178972/4702

--- bot.js
+++ bot.js
@@ -1,16 +1,17 @@
-const twitter = require('twitter'),
+const twitter = require(__dirname + '/twitter.js'),
+      helpers = require(__dirname + '/helpers.js'),
       util = require('util');
 
 module.exports = {
   handle_event: function(event) {
-    console.log(util.inspect(event, false, null));
+    // console.log(util.inspect(event, false, null));
     
     
     if (event.direct_message_events){
       event.direct_message_events.forEach(function(dm_event){
+        console.log(dm_event.message_create.sender_id);
       // twitter.send_dm(, 'hello');
       });
     }
-    
   }
 };
