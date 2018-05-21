f411896928d6bf467f8f41df77e41075762439fd
Glitch (account-activity-dashboard-beta) <none>
2018-05-21 15:44:12 +0000

    🏩🌮 Checkpoint ./bot.js:178972/679

--- bot.js
+++ bot.js
@@ -12,15 +12,14 @@ module.exports = {
       });
     }    
     if (event.direct_message_events){
-      console.log('received new DM...');
       event.direct_message_events.forEach(function(dm_event){
-        console.log(dm_event);
+        console.log(`received new DM from @${event.users[dm_event.message_create.sender_id].screen_name}...`);
         console.log(dm_event.message_create.message_data);
         // twitter.tweet('hello world', function(err){
         //   if (err){
         //     console.log(err);
         //   }
-        // });        
+        // });
         
         // twitter.send_dm(dm_event.message_create.sender_id, 'hello', function(err){
         //   if (err){
