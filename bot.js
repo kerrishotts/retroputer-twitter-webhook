6964458205797c4e1fde933025df4ca4406dd673
Glitch (account-activity-dashboard-beta) <none>
2018-05-21 16:15:35 +0000

    💨🎖 Checkpoint ./bot.js:178972/1765

--- bot.js
+++ bot.js
@@ -4,30 +4,28 @@ const twitter = require(__dirname + '/twitter.js'),
 
 module.exports = {
   handle_event: function(event) {
-    console.log(util.inspect(event, false, null));    
-
+    console.log(util.inspect(event, false, null));
     
-  
     if (event.direct_message_indicate_typing_events){
       event.direct_message_indicate_typing_events.forEach(function(typing_event){
-          console.log(`@${event.users[typing_event.sender_id].screen_name} is typing...`);
+        var user_typing = event.users[typing_event.sender_id].screen_name;
+        
+          console.log(`@${user_typing} is typing...`);
       });
     }    
     if (event.direct_message_events){
       event.direct_message_events.forEach(function(dm_event){
-        console.log(`received new DM from @${event.users[dm_event.message_create.sender_id].screen_name}...`);
-        console.log(dm_event.message_create.message_data);
-        // twitter.tweet('hello world', function(err){
-        //   if (err){
-        //     console.log(err);
-        //   }
-        // });
-        
-        twitter.send_dm(dm_event.message_create.sender_id, 'hello', function(err){
-          if (err){
-            console.log(err);
-          }
-        });
+        var dm_sender = event.users[dm_event.message_create.sender_id].screen_name;
+        if (dm_sender !== process.env.BOT_USERNAME){
+          console.log(`received new DM from @${dm_sender}...`);
+          console.log(dm_event.message_create.message_data);
+
+          twitter.send_dm(dm_event.message_create.sender_id, 'hello', function(err){
+            if (err){
+              console.log(err);
+            }
+          });            
+        }
       });
     }
   }
