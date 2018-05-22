432200ebbc9ca11c01876245dcc8b82913935b47
Glitch (account-activity-dashboard-beta) <none>
2018-05-22 17:34:32 +0000

    👨‍❤️‍👨⏱ Checkpoint ./twitterbot.js:178972/1081 ./app.js:178972/542

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const twitterbot = require('./twitterbot');
 
 twitterbot.on('direct_message_events', function(event){
-    console.log('direct_message_events', event);    
+    twitterbot.send_dm(event.sender_id, 'hello', function(err){
+      if (err){
+        console.log(err);
+      }
+    });
 });
-
 
 const dashboard = require('./dashboard')(twitterbot);
--- twitterbot.js
+++ twitterbot.js
@@ -129,15 +129,15 @@ module.exports = {
   },
   handle_event: function(event){
     var bot_behavior = this.bot_behavior;
-    console.log(util.inspect(event, false, null));
+    // console.log(util.inspect(event, false, null));
     
     if (event.direct_message_indicate_typing_events){
       event.direct_message_indicate_typing_events.forEach(function(typing_event){
         var user_typing = event.users[typing_event.sender_id].screen_name;
           if (user_typing !== process.env.BOT_USERNAME){
             console.log(`@${user_typing} is typing...`);
-            if (bot_behavior[event]){
-              bot_behavior[event].forEach(function(fn){
+            if (bot_behavior['direct_message_indicate_typing_events']){
+              bot_behavior['direct_message_indicate_typing_events'].forEach(function(fn){
                 fn(event);
               });
             }
@@ -148,22 +148,13 @@ module.exports = {
       event.direct_message_events.forEach(function(dm_event){
         var dm_sender = event.users[dm_event.message_create.sender_id].screen_name;
         if (dm_sender !== process.env.BOT_USERNAME){
-
           console.log(`received new DM from @${dm_sender}...`);
-          console.log(dm_event.message_create.message_data);
-
-          if (bot_behavior[event]){
-            bot_behavior[event].forEach(function(fn){
+          
+          if (bot_behavior['direct_message_events']){
+            bot_behavior['direct_message_events'].forEach(function(fn){
               fn(dm_event.message_create);
             });
           }
-          
-          // twitter.send_dm(dm_event.message_create.sender_id, 'hello', function(err){
-          //   if (err){
-          //     console.log(err);
-          //   }
-          // });
-
         }
       });
     }    
