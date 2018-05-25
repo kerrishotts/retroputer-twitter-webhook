19f95db2b259e9b72d5185cb414be3a2fcf3c2f4
Glitch (account-activity-dashboard-beta) <none>
2018-05-25 18:45:40 +0000

    📌🎮 Checkpoint ./app.js:178972/325

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const twitterbot = require('./twitterbot');
 
-twitterbot.on('favorite_events', function(favorited_status, user){
-  /*
-    See what a tweet object looks like:
-    https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/intro-to-tweet-json
-  */
-  console.log({favorited_status, user});
-});
+/*
+  See code samples inside the examples folder. Happy tweeting!
+*/
 
 const dashboard = require('./dashboard')(twitterbot);
