8d9f1015f28c03e8b6e10436bf19b2d30e35f6df
Glitch (account-activity-dashboard-beta) <none>
2018-05-25 17:11:05 +0000

    🛬💴 Checkpoint ./app.js:178972/345 ./examples/tweet-respond.js:178972/2068

--- examples/tweet-respond.js
+++ examples/tweet-respond.js
@@ -8,12 +8,14 @@ twitterbot.on('tweet_create_events', function(tweet){
     Documentation for POST statuses/update:
     https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/post-statuses-update.html
   */
-  // console.log({tweet});
   
   var text = 'hello 👋';
+  
+/*
+  You can look at tweet.text to and change your bot's response based on the text from the tweet.
+*/  
 
   twitterbot.twit.post('statuses/update', {
-    // status: `@${tweet.user.screen_name} ${text}`,
     status: text,
     in_reply_to_status_id: tweet.id_str,
     auto_populate_reply_metadata: true
@@ -22,25 +24,6 @@ twitterbot.on('tweet_create_events', function(tweet){
       console.log('Error', err);
     }
   });  
-});
-
-
-twitterbot.on('follow_events', function(follower){
-  /*
-    See what a 'follower' user object looks like:
-    https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/user-object.html
-  */
-  console.log({follower});
-});
-
-
-
-twitterbot.on('favorite_events', function(favorited_status, user){
-  /*
-    See what a tweet object looks like:
-    https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/intro-to-tweet-json
-  */
-  console.log({favorited_status, user});
 });
 
 const dashboard = require('./dashboard')(twitterbot);
